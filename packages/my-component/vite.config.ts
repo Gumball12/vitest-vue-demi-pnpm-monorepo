718af53f45acd5f9f94b73cc7827972733269068
Gumball12 <to@shj.rip>
2022-10-23 20:19:33 +0900

    chore: exclude env.d.ts from my-component

--- packages/my-component/vite.config.ts
+++ packages/my-component/vite.config.ts
@@ -4,7 +4,12 @@ import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [
+    vue(),
+    dts({
+      exclude: ['src/env.d.ts'],
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
