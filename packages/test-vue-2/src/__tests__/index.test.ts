33547a3e7d0f2d466d38ce48632b4e81a94f735b
Gumball12 <to@shj.rip>
2022-11-20 04:24:21 +0900

    refactor: rename test to App

--- packages/test-vue-2/src/__tests__/index.test.ts
+++ packages/test-vue-2/src/__tests__/index.test.ts
@@ -1,10 +1,10 @@
 import { mount } from '@vue/test-utils';
 import { describe, expect, it } from 'vitest';
-import test from '../index.vue';
+import App from '../index.vue';
 
 describe('test vue 2', () => {
   it('check text', () => {
-    const wrapper = mount(test);
+    const wrapper = mount(App);
     expect(wrapper.text()).toBe('from vue component');
   });
 });
